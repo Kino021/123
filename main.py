cb73ca5b8e9105516ae767278698eaf26ce3d889
Kino021 <kpilustrisimo@spmadridlaw.com>
2025-03-18 13:55:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.title('Daily Remark Summary')
+st.title('DIALER PRODUCTIVITY PER CRITERIA OF BALANCE')
 
 @st.cache_data
 def load_data(uploaded_file):
